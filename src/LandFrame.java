808e065048360cf2c75b0c058fefd9835aacb362
Benjamin DosSantos <BlackPenSoftware@Gmail.com>
2016-03-17 16:49:27 -0400

    First push of old projects to GitHub

---
+++ src/LandFrame.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class LandFrame extends Applet{
+	Graphics graphics;
+	
+	public void init(){
+		
+	}// End of init method
+	
+	public void paint(Graphics g){
+		graphics = g;
+	}// End of paint method
+}// End of class
